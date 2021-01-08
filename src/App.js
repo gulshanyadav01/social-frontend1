e8ccf147ed8a504587d224c663fbde783641184d
gulshanyadav01 <gulshany01@gmail.com>
2021-01-08 15:48:39 +0530

    private route is done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import  store  from "./components/store/store";
 import Alert from "./components/Layout/alert"; 
 import { loadUser } from "./components/store/Actions/auth"
 import setAuthToken from "./utils/setAuthToken";
-import {BrowserRouter as Router, Switch} from "react-router-dom"; 
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"; 
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/Routing/PrivateRoute"; 
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -21,15 +23,14 @@ const  App = () =>  {
   },[])
   return (
     <Provider store = {store}>
-    <Router>
-    <Navbar/>
-    <Landing/>
-    <Switch>
-    <h1>hello</h1>
-    <Alert/>
-    
-    </Switch>
-    </Router>
+      <Router>
+        <Navbar/>
+        <Landing/>
+        <PrivateRoute exact path = "/dashboard" component = {Dashboard}/>
+        <Switch>
+          <Alert/>
+        </Switch>
+      </Router>
     </Provider>
   );
 }
--- src/components/Layout/Landing.js
+++ src/components/Layout/Landing.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Register from "../auth/Register"; 
 import Login from "../auth/Login"
+import {Route } from "react-router-dom"; 
 const  Landing = () =>  {
     return (
         <div>
             <h1>Landing Page is Ok </h1>
-            <Register/>
-            <Login/>
+            <Route exact path = "/register" component = {Register} />
+            <Route exact path = "/login" component = {Login} />
         </div>
     )
 }
