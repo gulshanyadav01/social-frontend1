607ade8c2d0ba6511daba26fca5aff7f97f141b8
gulshanyadav01 <gulshany01@gmail.com>
2021-01-11 12:44:32 +0530

    get repos and get all profiles is done

--- src/components/store/Actions/profile.js
+++ src/components/store/Actions/profile.js
@@ -5,15 +5,20 @@ import {
     PROFILE_ERROR,
     UPDATE_PROFILE,
     ACCOUNT_DELETED,
-    CLEAR_PROFILE
+    CLEAR_PROFILE,
+    GET_PROFILES,
+    GET_REPOS
 } from "./Types";
 
 // GET THE CURRENT USER 
 
 export const getCurrentProfile = () => async dispatch => {
+    dispatch({
+        type:CLEAR_PROFILE
+    })
     try{
         const res = await axios.get("http://localhost:5000/api/profile/me"); 
-
+        
         dispatch({
             type:GET_PROFILE,
             payload: res.data
@@ -28,8 +33,60 @@ export const getCurrentProfile = () => async dispatch => {
     }
 }
 
-// create or update the profile
+// get all profiles 
+export const getProfiles = () => async dispatch => {
+    try{
+        const res = await axios.get("http://localhost:5000/api/profile");
+        dispatch({
+            type:GET_PROFILES,
+            payload: res.data
+        })
 
+    }catch(err){
+        dispatch({
+            type:PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+// get profile by id 
+export const getProfilieById = (userId) => async dispatch => {
+    try{
+        const res = await axios.get(`http://localhost:5000/api/profile/user/${userId}`)
+        dispatch({
+            type:GET_PROFILE,
+            payload:res.data
+        })
+
+    }catch(err){
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+}
+ 
+// get github repos
+export const getGithubRepos = username => async dispatch => {
+    try{
+        const res = await axios.get(`http://localhost:5000/api/profile/github/${username}`)
+        dispatch({
+            type:GET_REPOS,
+            payload: res.data
+        })
+
+    }catch(err){
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+
+    }
+}
+
+
+// create or update the profile
 export const createProfile = (formData, history, edit = false) => async dispatch  => {
     try {
         const config = {
@@ -198,3 +255,5 @@ export const deleteAccount = (history) => async dispatch => {
     }
     
 }
+
+
