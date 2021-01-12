dd0a1f7e428f1a174875297ee5ade1b34517d6e2
gulshanyadav01 <gulshany01@gmail.com>
2021-01-12 13:14:05 +0530

    post reducer is done

--- src/components/store/Reducers/post.js
+++ src/components/store/Reducers/post.js
@@ -1,7 +1,33 @@
-// import { }
-// const initialState = {
-//     posts:[],
-//     post:null,
-//     loading:true,
-//     error:{}
-// }
+import { 
+    GET_POSTS,
+    POST_ERROR
+} from "../Actions/Types";
+
+const initialState = {
+    posts:[],
+    post:null,
+    loading:true,
+    error:{}
+}
+
+export default function post (state = initialState , action  ){
+    const { type, payload } = action;
+    switch(type){
+        case GET_POSTS:
+            return {
+                ...state,
+                posts: payload,
+                loading: false
+            }
+        case POST_ERROR:
+            return{
+                ...state,
+                error: payload,
+                loading: false
+
+            }
+        default:
+            return state
+    }
+
+}
